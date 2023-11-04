7c028aafc30e7b7cdd9cec4525a14474013e9f98
NancyMungai <nancy.mungai@strathmore.edu>
2023-11-04 17:50:13 +0300

    Added bootstrap template

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get ('home',[HomeController::class,'index'])->name('home');
+Route::get ('about',[HomeController::class,'about'])->name('about');
+
 
 Route::get('/', function () {
     return view('welcome');
