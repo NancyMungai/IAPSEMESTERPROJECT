ecdf8a97a0c512ea7f98563b00385697a24d2946
christinekeni <christinekeni@strathmore.edu>
2023-11-21 20:45:25 +0300

    fixed login ui

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,17 +1,13 @@
 @extends('layout')
 
 @section('content')
-    <!-- Your Login Form Content Goes Here -->
     <div class="container" style="margin-top: 100px; margin-bottom: 10px;">
-        <!-- Your Login Form HTML -->
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ __('Login') }}</div>
 
                     <div class="card-body">
-
-
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
@@ -68,38 +64,18 @@
                                     @endif
                                 </div>
                             </div>
-                             <!-- Remember Me and Login Button -->
 
-                            <!-- Link to Register -->
                             <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-
-
-                                    <!-- Register Link -->
-                                    <p class="mt-3 mb-0">
+                                <div class="col-md-8 offset-md-4 mt-3">
+                                    <p>
                                         Don't have an account? <a href="{{ route('register') }}">Register here</a>.
                                     </p>
                                 </div>
                             </div>
                         </form>
-
                     </div>
                 </div>
-@section('slot')
-<div class="container mx-auto mt-8">
-    <div class="w-full max-w-xs m-auto">
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="{{ route('login') }}">
-            @csrf
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-                    Email Address
-                </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" name="email" required autofocus placeholder="Enter your email">
             </div>
         </div>
     </div>
-    <!-- End of Your Login Form Content -->
-
-    <!-- Your Remaining Content -->
-    <!-- For instance, additional sections or scripts -->
 @endsection
