8df3327a48e41d7e301b2f9e6c267a9dbb5c83ee
VCyrilla <vanessa.cyrilla@strathmore.edu>
2023-11-23 16:35:36 +0300

    Private environment variables for MPESA API

--- config/app.php
+++ config/app.php
@@ -184,5 +184,20 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
+    
+    /*Secure M-PESA environment variables*/
+    'debug_blacklist' => [
+        '_ENV' =>[
+            'MPESA_CONSUMER_KEY',
+            'MPESA_CONSUMER_SECRET',
+            'MPESA_SHORTCODE',
+            'MPESA_STK_SHORTCODE',
+            'MPESA_ENV',
+            'MPESA_TEST_MSISDN',
+            'MPESA_TEST_URL',
+            'MPESA_PASSKEY',
+            'MPESA_B2C_PASSWORD'
+        ]
+    ]
 
 ];
