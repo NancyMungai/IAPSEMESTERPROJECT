e3d7655ef06f36d994f1d65367269346ebea7ac5
VCyrilla <vanessa.cyrilla@strathmore.edu>
2023-11-27 20:39:24 +0300

    edits to MPESAController & checkout to find MpesaURL

--- app/Http/Controllers/payments/mpesa/MPESAController.php
+++ app/Http/Controllers/payments/mpesa/MPESAController.php
@@ -7,6 +7,25 @@ use Illuminate\Http\Request;
 
 class MPESAController extends Controller
 {
+    public function getAccessToken(){
+        $url = env('MPESA_ENV') = 0
+        ?''
+        :'';
+        $curl = curl_init($url);
+        curl_setopt_array(
+            $curl,
+         array(
+CURLOPT_HTTPHEADER => ['Content-Type: application/json; charset =utf8'],
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_HEADER => false,
+CURLOPT_USERPWD => env('MPESA_CONSUMER_KEY') . ':' . env('MPESA_CONSUMER_SECRET')
+         )
+         );
+         $response = json_decode(curl_exec($curl));
+         curl_close($curl);
+         return $response->access_token;
+        
+    }
     /**
      * Display a listing of the resource.
      */
--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -145,7 +145,64 @@
 							</tbody>
 						</table>
 						<a href="#" class="boxed-btn">Place Order</a>
+
+						<div class= "container">
+							<div class= "row">
+								<div class= "col-sm-8 mx-auto">
+									<div class="card">
+										<div class= "card-header">
+											Obtain Access Token
+										</div>
+										<div class= "card-body">
+											<button id="getAccessToken" class="btn btn-primary">Request Access Token</button>
+										</div>
+									</div>
+
+									<div class="card">
+										<div class= "card-header">Register URLs</div>
+										<div class= "card-body">
+										<button class="btn btn-primary">Register URLs</button>
+
+										</div>
+									</div>
+
+									<div class="card">
+										<div class= "card-header">Simulate Transaction</div>
+										<div class= "card-body">
+											<form action= "">
+												@csrf
+												 <div class= "form-group">
+													<label for="amount">Amount</label>
+													<input type="number" name="amount" class="form-control" id="amount">
+												  </div>
+												  <div class= "form-group">
+													<label for="account">Account</label>
+													<input type="text" name="account" class="form-control" id="account">
+												  </div>
+                                                  <button class="btn btn-primary">Simulate Payment</button>
+										      </form>
+											</div>
+									   </div>
+
+									
+									</div>
+							</div>
+
 					</div>
+					<script src="{{ asset ('js/app.js') }}"></script>
+<script>
+	document.getElementById('getAccessToken').addEventListener('click', (event) =>{
+		event.preventDefault()
+
+		axios.post('/get-token', {})
+		.then((response ) => {
+			console.log(response);
+		})
+		.catch((error) =>{
+			console.log(error);
+		})
+	})
+</script>
 				</div>
 			</div>
 		</div>
