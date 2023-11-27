f5acdab7157eba490c8c29c73ab54e368a679700
VCyrilla <vanessa.cyrilla@strathmore.edu>
2023-11-27 21:08:28 +0300

    updates

--- app/Http/Controllers/payments/mpesa/MPESAController.php
+++ app/Http/Controllers/payments/mpesa/MPESAController.php
@@ -8,9 +8,7 @@ use Illuminate\Http\Request;
 class MPESAController extends Controller
 {
     public function getAccessToken(){
-        $url = env('MPESA_ENV')==0
-        ?''
-        :'';
+        $url = env('MPESA_ENV') == 0 ?'' :'';
         $curl = curl_init($url);
         curl_setopt_array(
             $curl,
