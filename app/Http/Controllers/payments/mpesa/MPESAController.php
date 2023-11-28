55519cc022560357d3df44dbfe4d23204fecc3d6
VCyrilla <vanessa.cyrilla@strathmore.edu>
2023-11-28 03:10:04 +0300

    more updates

--- app/Http/Controllers/payments/mpesa/MPESAController.php
+++ app/Http/Controllers/payments/mpesa/MPESAController.php
@@ -10,7 +10,7 @@ class MPESAController extends Controller
     public function getAccessToken(){
         $url = env('MPESA_ENV') == 0 
         ?'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
-       :''; /*didn't add live*/
+       :''; //perhaps add live
        $curl = curl_init($url);
         curl_setopt_array(
             $curl,
@@ -24,8 +24,46 @@ CURLOPT_USERPWD => env('MPESA_CONSUMER_KEY') . ':' . env('MPESA_CONSUMER_SECRET'
          $response = curl_exec($curl);
          curl_close($curl);
          return $response;
-        
+        //return $response->access_token;*\snmp_set_oid_numeric_print
+
     }
+
+    public function registerURLS(){
+$body = array(
+    'Shortcode' => env('MPESA_SHORTCODE'),
+    'ResponseType' => 'Completed',
+    'ConfirmationURL' =>env('MPESA_TEST_URL') . '/api/confirm/dsxcv',
+    'ValidationURL' => env('MPESA_TEST_URL') . '/api/validation'
+);
+$url = env('MPESA_ENV') == 0 
+        ?'https://sandbox.safaricom.co.ke/mpesa/c2b/v3/registerurl'
+       :''; //perhaps add live
+
+$response = $this->makeHttp($url, $body);
+
+}
+
+public function makeHttp($url, $body)
+{
+    $curl = curl_init();
+    curl_setopt_array(
+        $curl,
+        array(
+           CURLOPT_URL => $url,
+           CURLOPT_HTTPHEADER => array('Content-Type:applicaqtion/json', 'Authorization:Bearer '. $this ->getAccessToken()), //Setting custom header
+           CURLOPT_RETURNTRANSFER => true,
+           CURLOPT_POST => true,
+           CURLOPT_POSTFIELDS => json_encode($body)
+     )
+    );
+$curl_response = curl_exec($curl);
+curl_close($curl);
+return $curl_response;
+
+
+
+}
+
     /**
      * Display a listing of the resource.
      */
