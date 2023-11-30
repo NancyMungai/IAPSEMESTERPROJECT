9530f28f7633276183976320973a687585ddbd8e
Shani001 <shanice.nyaga@strathmore.edu>
2023-11-30 13:56:16 +0300

    product page and cart page update

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -395,4 +394,3 @@ class ProductSeeder extends Seeder
 
     }
 
-}
